d4c47aaaa1ff5537ca8c9eb02cb6c06543b423a6
SalimVazquez <181086@ids.upchiapas.edu.mx>
2021-02-05 23:26:42 -0600

    add validation in login form

--- lib/main.dart
+++ lib/main.dart
@@ -53,8 +53,63 @@ class Welcome extends StatelessWidget {
 }
 
 class Login extends StatelessWidget {
+  final _username = TextEditingController();
+  final _password = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    Future<void> _validateForm(String username, String password) async {
+      return showDialog<void>(
+        context: context,
+        barrierDismissible: false, // user must tap button!
+        builder: (BuildContext context) {
+          if (username.toString().isEmpty ||
+              username == null ||
+              password.toString().isEmpty ||
+              password == null) {
+            return AlertDialog(
+              title: Text('AppRoot'),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    Text('Error'),
+                    Text('Es necesario ingresar los datos!'),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('Retry!'),
+                  onPressed: () {
+                    Navigator.of(context, rootNavigator: false).pop();
+                  },
+                ),
+              ],
+            );
+          } else {
+            return AlertDialog(
+              title: Text('AppRoot'),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    Text('Hello'),
+                    Text('Welcome $username'),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('OK!'),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/index');
+                  },
+                ),
+              ],
+            );
+          }
+        },
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('AppRoot'),
@@ -67,6 +122,7 @@ class Login extends StatelessWidget {
         Padding(
           padding: EdgeInsets.fromLTRB(35, 205, 35, 0),
           child: TextField(
+            controller: _username,
             obscureText: false,
             decoration: InputDecoration(
                 contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
@@ -79,6 +135,7 @@ class Login extends StatelessWidget {
         Padding(
             padding: EdgeInsets.fromLTRB(35, 30, 35, 0),
             child: TextField(
+              controller: _password,
               obscureText: true,
               decoration: InputDecoration(
                   contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
@@ -94,7 +151,7 @@ class Login extends StatelessWidget {
                   backgroundColor:
                       MaterialStateProperty.all<Color>(Colors.red)),
               onPressed: () {
-                Navigator.pushNamed(context, '/index');
+                _validateForm(_username.text, _password.text);
               },
               icon: Icon(Icons.verified_user, size: 18),
               label: Text("Iniciar sesión"),
